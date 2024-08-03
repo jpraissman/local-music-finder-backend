551a09fde2d10016f9a0af84677e0280ebedd2ca
Jordan <jordanpraissman@gmail.com>
2024-08-02 20:04:59 -0400

    Made sure env variables were correct

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ import os
 
 # Create important server stuff
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+database_url = os.environ.get('DATABASE_URL')
+if database_url.startswith("postgres:"):
+  database_url = database_url.replace("postgres:", "postgresql:", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
