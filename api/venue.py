5dd72a78ed6b0cbf460fbf91c1f9d42a92e9a77e
Jordan <jordanpraissman@gmail.com>
2025-05-23 23:29:54 -0400

    Add getting events at a specific venue

--- api/venue.py
+++ api/venue.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify
 from scripts.models.venue import Venue
+from scripts.models.event import Event
+from app import db
+from datetime import datetime
 
 venue_bp = Blueprint('venue', __name__)
 
@@ -13,3 +16,15 @@ def get_all_venues():
     response[venue.venue_name] = {"address": venue.address, "place_id": venue.place_id}
 
   return jsonify(response)
+
+@venue_bp.route('/venue/<venue_name>/events', methods = ['GET'])
+def get_venue_events(venue_name):
+  events = db.session.query(Event).join(Event.venue).filter(Venue.venue_name == venue_name).all()
+
+  events_json = []
+  for event in events:
+    event.set_distance_data("", -1)
+    events_json.append(event.get_all_details(False, False))
+
+  events_json_sorted = sorted(events_json, key=lambda x: datetime.fromisoformat(x["event_datetime"]))
+  return {'events': events_json_sorted}
