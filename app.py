ca19031d94a027ce5012b4c521f67623c77afdb5
Jordan Praissman <90515336+jpraissman@users.noreply.github.com>
2025-08-23 19:39:19 -0400

    Add robots.txt (#12)

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ import sentry_sdk
 
 # Important server stuff
 app = Flask(__name__)
-database_url = os.environ.get('DATABASE_URL')
+database_url = os.environ.get("DATABASE_URL")
 if database_url.startswith("postgres:"):
-  database_url = database_url.replace("postgres:", "postgresql:", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    database_url = database_url.replace("postgres:", "postgresql:", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 CORS(app)
@@ -23,13 +23,13 @@ executor = Executor(app)
 limiter = Limiter(app=app, key_func=lambda: "global", default_limits=["100 per minute"])
 
 if not app.config.get("TESTING"):
-  sentry_sdk.init(
-      dsn = os.environ.get('SENTRY_DSN'),
-  )
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_DSN"),
+    )
 
 # Get API Keys
-API_KEY = os.environ.get('API_KEY')
-ADMIN_KEY = os.environ.get('ADMIN_KEY')
+API_KEY = os.environ.get("API_KEY")
+ADMIN_KEY = os.environ.get("ADMIN_KEY")
 
 # Must be imported after to avoid circular import
 from api.event import event_bp
@@ -63,9 +63,11 @@ from scripts.models.bot_activity import BotActivity
 from scripts.models.video_click import VideoClick
 from scripts.models.email_creds import EmailCreds
 
+
 # Used to make helper send rate limit emails
 class RateLimitEmailHelper:
-  email_sent = False
+    email_sent = False
+
 
 # Custom 404 error handler
 @app.errorhandler(404)
@@ -76,59 +78,67 @@ def not_found(error):
     # Return an empty response
     return Response(status=200)
 
+
 @app.errorhandler(Exception)
 def handle_exception(e):
-  if not app.config.get("TESTING"):
-    sentry_sdk.capture_exception(e)
+    if not app.config.get("TESTING"):
+        sentry_sdk.capture_exception(e)
 
-  if app.debug:
-    raise e
-  
-  print("Handling Error")
+    if app.debug:
+        raise e
 
-  tb = traceback.format_exc()
+    print("Handling Error")
 
-  request_info = {
-    "method": request.method,
-    "url": request.url,
-    "headers": dict(request.headers),
-    "body": request.get_data(as_text=True)
-  }
+    tb = traceback.format_exc()
 
-  response = {
-    "error": {
-      "type": type(e).__name__,
-      "message": str(e),
-      "traceback": tb
-    },
-    "request": request_info,
-    "path": request.path
-  }
+    request_info = {
+        "method": request.method,
+        "url": request.url,
+        "headers": dict(request.headers),
+        "body": request.get_data(as_text=True),
+    }
 
-  EmailSender.send_error_occurred_email(json.dumps(response, indent=8))
-  return jsonify(response), 500
+    response = {
+        "error": {"type": type(e).__name__, "message": str(e), "traceback": tb},
+        "request": request_info,
+        "path": request.path,
+    }
+
+    EmailSender.send_error_occurred_email(json.dumps(response, indent=8))
+    return jsonify(response), 500
+
 
 # Handles rate limit errors by sending an email alerting that a rate limit has been hit
 @app.errorhandler(429)
 def handle_rate_limit_exception(e):
-  print("Handling Rate Limit Error")
-  if not RateLimitEmailHelper.email_sent:
-    EmailSender.send_error_occurred_email("The API limit was hit: Too many requests in the last minute have been made (Over 100 requests).")
-    RateLimitEmailHelper.email_sent = True
-    executor.submit(reset_rate_limit_email)
-    print("Sent email and submitted reset request")
-  
-  return jsonify("Too many requests have been made in the last minute"), 429
+    print("Handling Rate Limit Error")
+    if not RateLimitEmailHelper.email_sent:
+        EmailSender.send_error_occurred_email(
+            "The API limit was hit: Too many requests in the last minute have been made (Over 100 requests)."
+        )
+        RateLimitEmailHelper.email_sent = True
+        executor.submit(reset_rate_limit_email)
+        print("Sent email and submitted reset request")
+
+    return jsonify("Too many requests have been made in the last minute"), 429
+
 
 # Resets rate limit email so emails can be sent again when the application hits a rate limit.
 def reset_rate_limit_email():
-  time.sleep(1200)
-  RateLimitEmailHelper.email_sent = False
-  print("Reset rate limit email")
+    time.sleep(1200)
+    RateLimitEmailHelper.email_sent = False
+    print("Reset rate limit email")
 
-@app.route('/health', methods=['GET'])
+
+@app.route("/health", methods=["GET"])
 def health_check():
-    return jsonify({'status': 'ok'}), 200
+    return jsonify({"status": "ok"}), 200
 
-if __name__ == '__main__':
-  app.run()
+
+@app.route("/robots.txt", methods=["GET"])
+def robots_txt():
+    return Response("User-agent: *\nDisallow: /", mimetype="text/plain")
+
+
+if __name__ == "__main__":
+    app.run()
